28f7c320dbdf3a83a93b66932e7f275f3ecd3a09
SammarTahir <G00347526@gmit.ie>
2018-08-12 13:17:47 +0100

    Main page done

---
+++ MobileAppProject/EatingOut.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MobileAppProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class EatingOut : Page
+    {
+        public EatingOut()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Fish(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RichFish(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void White(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Red(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Smoked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Green(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Roasted(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
